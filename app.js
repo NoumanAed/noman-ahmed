69b9db018f5d36bca1650d12d454b751010e1cca
ammar <ammarlala365@gmail.com>
2021-07-08 18:43:41 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,168 @@
+// Chapter no 01
+// 1. Write a script to greet your website visitor using JS alert
+// box.
+// 2. Write a script to display following message on your web
+// page:
+// Answer 
+// alert("Error! please enter a valid password");
+
+
+
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+// Answer
+        // alert("Welcome to JS Land... \n Happy Coding!");
+
+
+
+// 4. Write a script to display following messages in sequence:
+// alert("Welcome to JS Land...");
+// alert("Happy Coding! \n  Prevent this page from creating additional dialogs")
+
+
+
+        // 5. Generate the following message through browser’s
+        // developer console:
+        // console.log("Hello World");  
+
+
+
+
+
+
+
+
+
+                                                                // CHAPTER NO 2  VARIABLE FOR STRING
+// 1. Declare a variable called username.
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+
+// Answer no 1
+//  var username;
+//  Answer no 2
+//  var myName = "Nouman AHmed";
+// Answer no 3
+//  var message ="Hello World";
+// alert(message);
+
+// Anawer no 4
+// var myName = "Nouman Ahmed";
+// var age = 21;
+// var course = "Ceritified Mobile Application Development";
+// alert(myName);
+// alert(age);
+// alert(course);
+
+// 5. Write a script to display the following alert using one JS
+// variable:
+//  Answer no 5
+// var pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(pizza);
+
+
+
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+// Answer no 6
+//  var email = "My email address is nomanhmed241@gmail.com";
+// alert(email);
+
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+// Answer no 7
+// var book = "i am trying to learn from the book A smarter way to learn JavaScript."
+// alert(book);
+
+// 8. Write a script to display this in browser through JS
+// Anser no 8
+// var a = "Yah! i can write HTML content through JavaScript";
+// document.write(a);
+
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+// Answer no 9
+// var a = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(a);
+
+
+
+
+                                                                  //   CHAPTER NO 3  VARIABLES FOR NUMBERS 
+
+// 1. Declare a variable called age & assign to it your age. Show your age in an alert box.  
+// var age = "i am 15 year old"
+// alert(age);                                                                                    
+
+
+
+
+// Answer no 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+// Answer no 3
+
+// var birthYear = "My birth year is 1990";
+// document.write(birthYear);
+
+
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+// Answer no 4
+
+// var name = "nouman ";
+// var item = "levis "
+// var quan  = "5 T-shirts "
+// var store = "Bata Store ";
+// var total = name + item + quan + store ;
+// document.write(total);
